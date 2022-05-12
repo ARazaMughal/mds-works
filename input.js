51cc1dac7d10bd7b1924a373eb416f8aa5f9624e
arazamughal <ahmadraza89789@gmail.com>
2022-05-12 19:31:22 +0500

    input msg

---
+++ input.js
@@ -0,0 +1,7 @@
+let namebtn = document.querySelector('button');
+namebtn.addEventListener('click', inputMsg);
+function inputMsg(){
+  
+        let name = prompt('Enter your name');
+        namesbtn.textcontent = +name;
+    }
